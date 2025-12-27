39060eef1a294fcb4ab204e3292a48806a60fde5
Qiubai-Git <wth110628@163.com>
2025-12-27 23:41:59 +0800

    消除与IEntityRenderDispatcherAccessor的mixin冲突问题

--- src/main/java/net/onixary/shapeShifterCurseFabric/mixin/EntityRenderDispatcherMixin.java
+++ src/main/java/net/onixary/shapeShifterCurseFabric/mixin/EntityRenderDispatcherMixin.java
@@ -4,16 +4,20 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.item.ItemRenderer;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Environment(EnvType.CLIENT)
 @Mixin(EntityRenderDispatcher.class)
 public abstract class EntityRenderDispatcherMixin implements IEntityRenderDispatcherAccessor {
+    @Final
     @Shadow
     private ItemRenderer itemRenderer;
 
+    /*与IEntityRenderDispatcherAccessor.java冲突
     public ItemRenderer getItemRenderer() {
         return this.itemRenderer;
     }
+     */
 }
